854c572eb679da960d48767d557b06714307829b
Prashant Sharma <pra@gmail.com>
2018-03-26 18:46:02 +0530

    sign_in target

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -18,8 +18,10 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
-
+   protected
+def after_sign_in_path_for(resource)
+  '/accounts'
+end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
