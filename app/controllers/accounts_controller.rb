7f30e5fce6ac2f1ed33765811f04a5f1f8c0734b
Prashant Sharma <pra@gmail.com>
2018-03-26 18:34:04 +0530

    adding multiple accounts to one user

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,15 @@
 class AccountsController < ApplicationController
+  before_action :authenticate_user!
+  def new
+    @account =Account.new
+  end
+  def create
+    @account = Account.new(account_params)
+    @account.user_id = current_user.id
+    @account.save
+  end
+  private
+    def account_params
+      params.require(:account).permit(:name,:user_id)
+    end
 end
