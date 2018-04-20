809e74a29b30c46797682b518947938b7eeb5db6
prashant <prashant080994@gmail.com>
2018-04-20 17:34:04 +0530

    worked on frontend

--- app/assets/javascripts/channels/room.js
+++ app/assets/javascripts/channels/room.js
@@ -1,32 +1,35 @@
 (function() {
-  jQuery(document).on('turbolinks:load', function() {
+  jQuery(document).on('turbolinks:load', function(){
     var messages;
     messages = $("#data").attr('data-value');
-    return App.room = App.cable.subscriptions.create({
-      channel: "RoomChannel",
-      id: messages
-    }, {
-      connected: function() {},
-      disconnected: function() {},
-      received: function(data) {
-        return $('#messages').append(data['message']);
+    if ($(".teams.show").length > 0) {
+      return App.room = App.cable.subscriptions.create( {
+        channel: "RoomChannel",
+        id: messages,
       },
-      speak: function(message, team_id) {
-        return this.perform('speak', {
-          message: message,
-          team_id: team_id
-        });
-      }
-    }, $(document).on('keypress', function(event) {
-      debugger
-      var message, team_id;
-      team_id = $("#data").attr('data-value');
-      message = $("#data").val();
-      if (event.keyCode === 13) {
-        App.room.speak(message, team_id);
-        event.target.value = '';
-        return event.preventDefault();
-      }
-    }));
+      {
+        connected: function() {},
+        disconnected: function() {},
+        received: function(data) {
+          return $('#messages').append(data['message']);
+        },
+        speak: function(message, team_id) {
+          return this.perform('speak', {
+            message: message,
+            team_id: team_id
+          });
+        }
+      },
+      $(document).on('keypress', function(event){
+       var message, team_id;
+        team_id = $("#data").attr('data-value');
+        message = $("#data").val();
+        if (event.keyCode === 13) {
+          App.room.speak(message, team_id);
+          event.target.value = '';
+          return event.preventDefault();
+        }
+      }));
+    }
   });
 }).call(this);
