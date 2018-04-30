726b6a55d7503da7168990332ea7a27ba8890fda
prashant <prashant080994@gmail.com>
2018-04-30 11:14:02 +0530

    30 april done comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,6 @@ class CommentsController < ApplicationController
         @comment = @commentable.comments.new comment_params
         @comment.user_id = current_user.id
         @comment.save
-
         redirect_to @commentable, notice: "Your comment was successfully posted."
     end
   end
@@ -15,7 +14,7 @@ class CommentsController < ApplicationController
       params.require(:comment).permit(:body)
     end
     def set_commentable
-      if params[:team_id]
+      if params[:team_id].present?
        @commentable = Team.find(params[:team_id])
      elsif params[:account_id].present?
         @commentable = Account.find(params[:account_id])
